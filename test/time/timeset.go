b9feeccd26f24418868addaee112a505a8e9e974
xenomech <gokulsureshkumarak@gmail.com>
2020-02-08 13:38:54 -0500

    set time

---
+++ test/time/timeset.go
@@ -0,0 +1,10 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+        fmt.Print("Enter the amount of time :  ")   //Print function is used to display output in same line
+        var time string    
+        fmt.Scanln(&time)                  // Take input from user
+        
+}
