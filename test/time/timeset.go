e1fccb2092f556af4f36fe8bffd300dc1749e09a
xenomech <gokulsureshkumarak@gmail.com>
2020-02-08 13:53:15 -0500

    postgettest

--- test/time/timeset.go
+++ test/time/timeset.go
@@ -6,5 +6,5 @@ func main() {
         fmt.Print("Enter the amount of time :  ")   //Print function is used to display output in same line
         var time string    
         fmt.Scanln(&time)                  // Take input from user
-        
+        fmt.Print("time:",time)
 }
