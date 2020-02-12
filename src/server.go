951c030a356d5dc7c72f81fb275d34879cdbbd74
mohitrajane <mhit98@gmail.com>
2020-02-12 10:40:38 +0530

    remove duplicate Process struct

--- src/server.go
+++ src/server.go
@@ -9,11 +9,6 @@ import (
 	"os"
 )
 
-// type Process struct{
-// 	Name string `json:"name"`
-// 	Time float64 `json:"time"`
-// }
-
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
@@ -22,7 +17,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 
-    case "GET" :
+	case "GET":
 
 		jsonFile, _ := os.Open("output.json")
 		user_data, err := ioutil.ReadAll(jsonFile)
@@ -39,76 +34,76 @@ func hello(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
 }
-func enable_keyboard(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Enabling keyboard")
-    if r.URL.Path != "/quit/" {
-        http.Error(w, "Unauthorized", http.StatusUnauthorized)
-        return
-    }
-    switch r.Method {
-    // case "POST":
-    //     fmt.Println("METHOD_POST!!!")
-    //     fmt.Println(r.Method)
-    //     var command_enable_keyboard = "xinput enable 17"
-    //     _ =exe_cmd(command_enable_keyboard,1)
-    case "GET":
-        fmt.Println("GET ENABLE")
-        var command_enable_keyboard = "xinput enable 17"
-        // var command_enable_keyboard = "ls -al"
-        _ =exe_cmd(command_enable_keyboard,1)
-        os.Exit(0)
-    default:
-        fmt.Println("METHOD!!!")
-        fmt.Println(r.Method)
-        http.Error(w, "404 not found. DEFAULT", http.StatusNotFound)
-    }
+func enable_keyboard(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Enabling keyboard")
+	if r.URL.Path != "/quit/" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	// case "POST":
+	//     fmt.Println("METHOD_POST!!!")
+	//     fmt.Println(r.Method)
+	//     var command_enable_keyboard = "xinput enable 17"
+	//     _ =exe_cmd(command_enable_keyboard,1)
+	case "GET":
+		fmt.Println("GET ENABLE")
+		var command_enable_keyboard = "xinput enable 17"
+		// var command_enable_keyboard = "ls -al"
+		_ = exe_cmd(command_enable_keyboard, 1)
+		os.Exit(0)
+	default:
+		fmt.Println("METHOD!!!")
+		fmt.Println(r.Method)
+		http.Error(w, "404 not found. DEFAULT", http.StatusNotFound)
+	}
 }
-func enable_only(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Enabling keyboard")
-    if r.URL.Path != "/enable/" {
-        http.Error(w, "Unauthorized", http.StatusUnauthorized)
-        return
-    }
-    switch r.Method {
-    case "GET":
-        fmt.Println("GET ENABLE_ONLY")
-        var command_enable_keyboard = "xinput enable 17"
-        _ =exe_cmd(command_enable_keyboard,1)
-    default:
-        fmt.Println("METHOD!!!")
-        fmt.Println(r.Method)
-        http.Error(w, "404 not found. DEFAULT", http.StatusNotFound)
-    }
+func enable_only(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Enabling keyboard")
+	if r.URL.Path != "/enable/" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	case "GET":
+		fmt.Println("GET ENABLE_ONLY")
+		var command_enable_keyboard = "xinput enable 17"
+		_ = exe_cmd(command_enable_keyboard, 1)
+	default:
+		fmt.Println("METHOD!!!")
+		fmt.Println(r.Method)
+		http.Error(w, "404 not found. DEFAULT", http.StatusNotFound)
+	}
 }
-func disable_keyboard(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Disabling keyboard")
-    if r.URL.Path != "/disable/" {
-        http.Error(w,"Unauthorized", http.StatusUnauthorized)
-        return
-    }
-    switch r.Method{
-    case "POST":
-        fmt.Println("IN POST_disable")
-        var command_disable_keyboard = "xinput disable 17"
-        _ = exe_cmd(command_disable_keyboard,1)
-    case "GET":
-        fmt.Println("IN GET_disable")
-        var command_disable_keyboard = "xinput disable 17"
-        _ = exe_cmd(command_disable_keyboard,1)
-    default:
-        http.Error(w,"404 not found.",http.StatusNotFound)
-    }
-} 
+func disable_keyboard(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Disabling keyboard")
+	if r.URL.Path != "/disable/" {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	case "POST":
+		fmt.Println("IN POST_disable")
+		var command_disable_keyboard = "xinput disable 17"
+		_ = exe_cmd(command_disable_keyboard, 1)
+	case "GET":
+		fmt.Println("IN GET_disable")
+		var command_disable_keyboard = "xinput disable 17"
+		_ = exe_cmd(command_disable_keyboard, 1)
+	default:
+		http.Error(w, "404 not found.", http.StatusNotFound)
+	}
+}
 
 func Server(c chan int) {
-    http.HandleFunc("/", hello)
-    http.HandleFunc("/quit/",enable_keyboard)
-    http.HandleFunc("/disable/",disable_keyboard)
-    http.HandleFunc("/enable/",enable_only)
-    fmt.Printf(" Server running at 5080...\n")
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/quit/", enable_keyboard)
+	http.HandleFunc("/disable/", disable_keyboard)
+	http.HandleFunc("/enable/", enable_only)
+	fmt.Printf(" Server running at 5080...\n")
 
-    if err := http.ListenAndServe(":5080", nil); err != nil {
-        log.Fatal(err)
-    }
-    c <- 1
+	if err := http.ListenAndServe(":5080", nil); err != nil {
+		log.Fatal(err)
+	}
+	c <- 1
 }
